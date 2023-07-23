33403744e99fdc1154131a5325222234c1080be6
chialingd3 <chialingd3@gmail.com>
2023-07-23 16:00:51 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'eec9abd5-ac61-491f-ad6c-c0d1e95e1b5a';
+export const MODAL_CLOSE_SEC = 2.5;
